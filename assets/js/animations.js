a71c20602855c7bc4871d299752f9ef84611a79c
Nick Jocius <capacity512@yahoo.com>
2021-09-06 17:31:30 -0500

    added slide fade in animation on load to Explore text

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -5,3 +5,29 @@
 //Author: Nick Jocius
 //Description: Custom javascript animations for Udundi Test Project
 //
+
+window.addEventListener('load', Explore);
+
+function Explore() {
+    
+    const explore = document.getElementById('Explore');
+
+    let slideIn = [
+        {opacity: '0%'},
+        { left: '-1000px' },
+        { left: '-500px' },
+        { left: '-250px' },
+        {opacity: '50%'},
+        { left: '0px' },
+        { left: '50px' },
+        { opacity: '100%' },
+        {left: '150px'}
+    ]
+
+    let slideTiming = {
+        fill: 'forwards',
+        duration: 2000
+    }
+
+    explore.animate(slideIn, slideTiming);
+}
