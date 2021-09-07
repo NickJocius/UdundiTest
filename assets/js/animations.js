46e74e98e241745607659fc33e63750ab21b771f
Nick Jocius <capacity512@yahoo.com>
2021-09-06 20:19:47 -0500

    added onclick for close btn and animations to shrink and fade in

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -12,6 +12,8 @@ const card = document.getElementById('Card');
 const exploreBtn = document.getElementById('exploreBtn');
 exploreBtn.addEventListener('click', divExpand);
 const exploreTitle = document.getElementById('exploreTitle');
+const closeBtn = document.getElementById('closeBtn');
+closeBtn.addEventListener('click', divShrink);
 
 function Explore() {
 
@@ -91,4 +93,59 @@ function divExpand() {
     })
     
 
+}
+
+function divShrink() {
+    let fadeIn = [
+        {opacity: '0%'},
+        {opacity: '50%'},
+        { opacity: '100%' },
+    ]
+    let fadeTiming = {
+        fill: 'forwards',
+        duration: 200,
+        easing: 'ease-in'
+    }
+    let cardShrink = [
+        
+        {
+            left: '10px',
+            height: '40rem',
+            width: '30rem',
+            opacity: '100%'},
+            {
+                left: '-10px',
+                height: '20rem',
+                width: '20rem',
+                opacity: '60%'
+            },
+        {
+            left: '-50px',
+            height: '10rem',
+            width: '10rem',
+            opacity: '20%'},
+        {
+            left: '-100px',
+            height: '5rem',
+            width: '5rem',
+            opacity: '10%'
+        },
+        {
+            left: '-200px',
+            height: '0rem',
+            width: '0rem',
+            opacity: '0%'}
+        
+    ]
+    let shrinkTiming = {
+        fill: 'forwards',
+        duration: 200,
+        easing: 'linear'
+    }
+
+    let shrinking = card.animate(cardShrink, shrinkTiming);
+    shrinking.finished.then(() => {
+        card.classList.add('collapse');
+        explore.animate(fadeIn, fadeTiming);
+    })
 }
